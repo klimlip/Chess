9844859d82f5c0dce19ec9c66899461f1154df71
klimlip <klimlip3@mail.ru>
2017-12-14 17:08:46 +0300

    //

---
+++ Chess/Figure.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Chess
+{
+    public enum Color
+    {
+        White,
+        Black
+    }
+    public class Figure
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Point Location { get; private set; }
+        public Color color;
+        public int stepCount;
+        public Player player { get; private set; }
+        public bool isFirstPlayer { get; private set; }
+
+
+        public Figure(int x, int y)
+        {
+            stepCount = 0;
+
+        }
+    }
+    class Pawn : Figure
+    {
+        
+    }
+}
---
+++ Chess/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class Player
+    {
+        public bool IsHuman { get; private set; }
+
+    }
+}
